20150488c68d9ac333aeadb40b7ac1fd27caf67b
murad <murad@MURAD>
2022-11-25 22:22:24 +0400

    Add project files.

---
+++ KSLR_R_FaceRecognitionsSystem/FaceRecognitionSystem.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace KSLR_R_FaceRecognitionsSystem
+{
+    partial class FaceRecognitionSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FaceRecognitionSystem));
+            this.cameraBox = new Emgu.CV.UI.ImageBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txName = new System.Windows.Forms.TextBox();
+            this.IBOutput = new Emgu.CV.UI.ImageBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblCountAllFaces = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.mic1 = new System.Windows.Forms.PictureBox();
+            this.mic2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IBOutput)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mic1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mic2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cameraBox
+            // 
+            this.cameraBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.cameraBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.cameraBox.Location = new System.Drawing.Point(15, 12);
+            this.cameraBox.Margin = new System.Windows.Forms.Padding(4);
+            this.cameraBox.Name = "cameraBox";
+            this.cameraBox.Size = new System.Drawing.Size(432, 303);
+            this.cameraBox.TabIndex = 2;
+            this.cameraBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 334);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 30);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Şəxsin Adı";
+            // 
+            // txName
+            // 
+            this.txName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txName.Location = new System.Drawing.Point(18, 368);
+            this.txName.Margin = new System.Windows.Forms.Padding(4);
+            this.txName.Name = "txName";
+            this.txName.Size = new System.Drawing.Size(290, 45);
+            this.txName.TabIndex = 6;
+            // 
+            // IBOutput
+            // 
+            this.IBOutput.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.IBOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.IBOutput.Location = new System.Drawing.Point(8, 174);
+            this.IBOutput.Margin = new System.Windows.Forms.Padding(4);
+            this.IBOutput.Name = "IBOutput";
+            this.IBOutput.Size = new System.Drawing.Size(137, 128);
+            this.IBOutput.TabIndex = 11;
+            this.IBOutput.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.IBOutput);
+            this.groupBox2.Controls.Add(this.lblName);
+            this.groupBox2.Controls.Add(this.lblCountAllFaces);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(476, 13);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(236, 310);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Nəticə:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(5, 147);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 21);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Sifətin şəkli";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(5, 15);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(192, 22);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "Kamerada olan şəxslər:";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.Blue;
+            this.lblName.Location = new System.Drawing.Point(5, 39);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(82, 24);
+            this.lblName.TabIndex = 16;
+            this.lblName.Text = "Heç kim";
+            // 
+            // lblCountAllFaces
+            // 
+            this.lblCountAllFaces.AutoSize = true;
+            this.lblCountAllFaces.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCountAllFaces.ForeColor = System.Drawing.Color.Red;
+            this.lblCountAllFaces.Location = new System.Drawing.Point(200, 116);
+            this.lblCountAllFaces.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCountAllFaces.Name = "lblCountAllFaces";
+            this.lblCountAllFaces.Size = new System.Drawing.Size(24, 25);
+            this.lblCountAllFaces.TabIndex = 15;
+            this.lblCountAllFaces.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1, 116);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 22);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Aşkarlanan üzlərin sayı :";
+            // 
+            // mic1
+            // 
+            this.mic1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.mic1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mic1.Image = ((System.Drawing.Image)(resources.GetObject("mic1.Image")));
+            this.mic1.Location = new System.Drawing.Point(773, 88);
+            this.mic1.Name = "mic1";
+            this.mic1.Size = new System.Drawing.Size(143, 145);
+            this.mic1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mic1.TabIndex = 13;
+            this.mic1.TabStop = false;
+            this.mic1.Click += new System.EventHandler(this.mic1_Click);
+            // 
+            // mic2
+            // 
+            this.mic2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mic2.Image = ((System.Drawing.Image)(resources.GetObject("mic2.Image")));
+            this.mic2.Location = new System.Drawing.Point(773, 88);
+            this.mic2.Name = "mic2";
+            this.mic2.Size = new System.Drawing.Size(143, 145);
+            this.mic2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mic2.TabIndex = 14;
+            this.mic2.TabStop = false;
+            this.mic2.Click += new System.EventHandler(this.mic2_Click);
+            // 
+            // FaceRecognitionSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(956, 434);
+            this.Controls.Add(this.mic1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.txName);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cameraBox);
+            this.Controls.Add(this.mic2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FaceRecognitionSystem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Üz tanıma sistemi";
+            this.Load += new System.EventHandler(this.FaceRecognitionSystem_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.cameraBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IBOutput)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mic1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mic2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txName;
+        public Emgu.CV.UI.ImageBox cameraBox;
+        public Emgu.CV.UI.ImageBox IBOutput;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblCountAllFaces;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox mic1;
+        private System.Windows.Forms.PictureBox mic2;
+    }
+}
+
---
+++ KSLR_R_FaceRecognitionsSystem/FaceRecognitionSystem.cs
@@ -0,0 +1,224 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KSLR_R_FaceRecognitionsSystem
+{
+    public partial class FaceRecognitionSystem : Form
+    {
+        //Variables 
+        MCvFont font = new MCvFont(FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
+
+        //HaarCascade Library
+        HaarCascade faceDetected;
+
+        //For Camera as WebCams 
+        Capture camera;
+
+        //Images List if Stored
+        Image<Bgr, Byte> Frame;
+
+        Image<Gray, byte> result;
+        Image<Gray, byte> TrainedFace = null;
+        Image<Gray, byte> grayFace = null;
+
+        //List 
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+
+        List<string> labels = new List<string>();
+        List<string> users = new List<string>();
+
+        int Count, NumLables, t;
+        string name, names = null;
+
+        SpeechRecognitionEngine reco = new SpeechRecognitionEngine();
+        SpeechSynthesizer cumle = new SpeechSynthesizer();
+
+        public FaceRecognitionSystem()
+        {
+            InitializeComponent();
+            faceDetected = new HaarCascade("haarcascade_frontalface_alt.xml");
+
+            try
+            {
+                string Labelsinf = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
+                string[] Labels = Labelsinf.Split(',');
+
+                NumLables = Convert.ToInt16(Labels[0]);
+                Count = NumLables;
+
+                string FacesLoad;
+
+                for (int i = 1; i < NumLables + 1; i++)
+                {
+                    FacesLoad = "face" + i + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + "/Faces/" + FacesLoad));
+                    labels.Add(Labels[i]);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Database Folder is empty..!, please Register Face");
+            }
+        }
+
+        private void mic1_Click(object sender, EventArgs e)
+        {
+            mic1.Visible = false;
+            reco.RecognizeAsync();
+        }
+
+        private void mic2_Click(object sender, EventArgs e)
+        {
+            mic1.Visible = true;
+        }
+
+        void SesimiTaniyarsa()
+        {
+            string[] komandalar = { "Kamera ac", "Saxla", "Baza ac" };
+            Choices secimler = new Choices(komandalar);
+            Grammar grammar = new Grammar(new GrammarBuilder(secimler));
+            reco.LoadGrammar(grammar);
+            reco.SetInputToDefaultAudioDevice();
+            reco.SpeechRecognized += SesimiTanidi;
+        }
+
+        private void SesimiTanidi(object sender, SpeechRecognizedEventArgs e)
+        {
+            mic1.Visible = true;
+            if (e.Result.Text == "Kamera ac")
+            {
+                cumle.SpeakAsync("Camera is opening");
+                camera = new Capture();
+                camera.QueryFrame();
+
+                Application.Idle += new EventHandler(FrameProcedure);
+
+                txName.Focus();
+            }
+            else if (e.Result.Text == "Saxla")
+            {
+                if (txName.Text == "" || txName.Text.Length < 2 || txName.Text == string.Empty)
+                {
+                    cumle.SpeakAsync("Please enter name of person");
+                }
+                else
+                {
+                    Count += 1;
+                    grayFace = camera.QueryGrayFrame().Resize(320, 240, INTER.CV_INTER_CUBIC);
+                    MCvAvgComp[][] DetectedFace = grayFace.DetectHaarCascade(faceDetected, 1.2, 10,
+                        HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+
+                    foreach (MCvAvgComp f in DetectedFace[0])
+                    {
+
+                        TrainedFace = Frame.Copy(f.rect).Convert<Gray, Byte>();
+                        break;
+                    }
+
+                    TrainedFace = result.Resize(100, 100, INTER.CV_INTER_CUBIC);
+
+                    trainingImages.Add(TrainedFace);
+                    IBOutput.Image = TrainedFace;
+
+                    labels.Add(txName.Text);
+
+                    File.WriteAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
+
+                    for (int i = 1; i < trainingImages.ToArray().Length + 1; i++)
+                    {
+                        trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/Faces/face" + i + ".bmp");
+                        File.AppendAllText(Application.StartupPath + "/Faces/Faces.txt", labels.ToArray()[i - 1] + ",");
+                    }
+
+                    cumle.SpeakAsync("Face Stored");
+                    txName.Text = "";
+                    txName.Focus();
+                }
+            }
+            else if (e.Result.Text == "Baza ac")
+            {
+                cumle.SpeakAsync("Database is opening");
+                Process.Start(new ProcessStartInfo()
+                {
+                    FileName = Application.StartupPath + "/Faces/",
+                    UseShellExecute = true,
+                    Verb = "open"
+                });
+            }
+        }
+
+        private void FaceRecognitionSystem_Load(object sender, EventArgs e)
+        {
+            SesimiTaniyarsa();
+        }
+
+        private void FrameProcedure(object sender, EventArgs e)
+        {
+            users.Add("");
+            lblCountAllFaces.Text = "0";
+
+            Frame = camera.QueryFrame().Resize(320, 240, INTER.CV_INTER_CUBIC);
+            grayFace = Frame.Convert<Gray, Byte>();
+
+            MCvAvgComp[][] faceDetectedShow = grayFace.DetectHaarCascade(faceDetected, 1.2, 10,
+                HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(20, 20));
+
+            foreach (MCvAvgComp f in faceDetectedShow[0])
+            {
+                t += 1;
+
+                result = Frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100, 100, INTER.CV_INTER_CUBIC);
+                Frame.Draw(f.rect, new Bgr(Color.Green), 3);
+
+                if (trainingImages.ToArray().Length != 0)
+                {
+                    MCvTermCriteria termCriterias = new MCvTermCriteria(Count, 0.001);
+                    EigenObjectRecognizer recognizer =
+                        new EigenObjectRecognizer(trainingImages.ToArray(),
+                        labels.ToArray(), 3000,
+                        ref termCriterias);
+
+                    name = recognizer.Recognize(result);
+                    Frame.Draw(name, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+
+                }
+                users[t - 1] = name;
+                users.Add("");
+                //Set the number of faces detected on the scene
+                lblCountAllFaces.Text = faceDetectedShow[0].Length.ToString();
+                users.Add("");
+
+            }
+
+            t = 0;
+
+            //Names concatenation of persons recognized
+            for (int nnn = 0; nnn < faceDetectedShow[0].Length; nnn++)
+            {
+                names = names + users[nnn] + ", ";
+            }
+
+            //Show the faces procesed and recognized
+            cameraBox.Image = Frame;
+            lblName.Text = names;
+            names = "";
+            users.Clear();
+        }
+
+    }
+
+}
